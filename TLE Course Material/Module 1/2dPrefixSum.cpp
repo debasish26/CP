471991a45648c25d9add8ae3cc185fafcdca7b82
Debasish Mahanta <debasish024h@gmail.com>
2025-06-06 21:17:45 +0530

    @d prefix sum

--- TLE Course Material/Module 1/2dPrefixSum.cpp
+++ TLE Course Material/Module 1/2dPrefixSum.cpp
@@ -1,35 +1,48 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-
+int main()
+{
     int n, m;
     cin >> n >> m;
+
     vector<vector<int>> v(n, vector<int>(m));
-    for (int i = 0; i < n;i++){
-        for (int j = 0; j < m;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             cin >> v[i][j];
         }
-
     }
-    vector<vector<int>> prefix(n, vector<int>(m));
-    for (int i = 0; i < n;i++){
-        for (int j = 0; j < m;j++){
+
+    vector<vector<int>> prefix(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             // prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + v[i][j];
             // it can give segmentation error if i or j becomes 0         }
             prefix[i][j] = v[i][j];
-            if(j>0){
+            if (j > 0)
                 prefix[i][j] += prefix[i][j - 1];
-            }
-            if(i>0){
+            if (i > 0)
                 prefix[i][j] += prefix[i - 1][j];
-            }
-            if(i>0 && j > 0){
+            if (i > 0 && j > 0)
                 prefix[i][j] -= prefix[i - 1][j - 1];
-            }
-            
+        }
     }
-        return 0;
+
+    // Optional: print the 2D prefix sum matrix
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << prefix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
