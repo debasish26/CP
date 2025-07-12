a7ce03682cb66c6110fc608a878442ae23b4aedc
debasish26 <debasish024h@gmail.com>
2025-07-12 09:12:42 +0530

    aur bhi gad faad math sawal

---
+++ TLE Course Material/Module 8/DifferentDivisors.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define nl '\n'
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+const ll mod = 1e9 + 7;
+
+// Binary exponentiation: (a^b) % mod
+ll expo(ll a, ll b) {
+    a %= mod;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+// Modular division: (a / b) % mod
+ll mod_div(ll a, ll b) {
+    return (a * expo(b, mod - 2)) % mod; // Fermat's Little Theorem (mod must be prime)
+}
+
+const int N = 1e6;
+bool prime[N];
+
+void sieve(){
+    for(int i = 0; i < N; i++){
+        prime[i] = 1;
+    }
+    prime[0] = prime[1] = 0;
+    for(int i = 2; i * i < N; i++){
+        if(prime[i]){
+            for(int j = i * i; j < N; j += i){
+                prime[j] = 0;
+            }
+        }
+    }
+}
+
+void solve() {
+    // Your logic here
+    int d;
+    cin>>d;
+    int p = 0;
+    for(int i = d+1;i<N;i++){
+        if(prime[i]){
+            p = i;
+            break;
+        }
+    }
+    int q = 0;
+    for(int i = p + d;i<N;i++){
+        if(prime[i]){
+            q = i;
+            break;
+        }
+    }
+    cout<<p*q<<nl;
+}
+
+int main() {
+    fast_io;
+    sieve();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    //solve();
+
+    return 0;
+}
